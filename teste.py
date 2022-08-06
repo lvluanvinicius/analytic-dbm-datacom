facc213efd6296579232658924c63177b0087de5
Luan Santos <44438249+lvluanvinicius@users.noreply.github.com>
2022-08-05 23:23:35 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,5 +0,0 @@
-with open("test", "r") as file: 
-    for line in file.readlines():
-        if "Rx Optical Power [dBm]" in line:
-            if "N/A" in line.split(":")[1].strip(): 
-                break
